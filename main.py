e866b38365da9c6ee9ec98dda44378715cc5eec6
raymag <prayk11@outlook.com>
2021-01-02 18:29:15 -0300

    .

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from textblob import TextBlob
 from flask import Flask, request
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/translate')
 def translate():
