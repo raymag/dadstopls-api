797fc93ad1b549e17e2a65687debc3b49b62d0b6
raymag <prayk11@outlook.com>
2021-01-02 18:23:29 -0300

    .

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ def translate():
     return str(translation)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=os.environ.get('DEV'), port=int(os.environ.get('PORT', '5000'))
+    app.run(host="0.0.0.0", debug=os.environ.get('DEV'), port=int(os.environ.get('PORT', '5000')))
